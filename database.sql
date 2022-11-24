4f00edcb16edc2d3249e2a29c6bd5052380d4a7b
Brianaldo <brianaldophandiarta31@gmail.com>
2022-11-24 18:21:17 +0700

    fix: database logging default value

--- database.sql
+++ database.sql
@@ -4,18 +4,18 @@ CREATE DATABASE `database`;
 
 CREATE TABLE `database`.`subscription`
 (
-    `creator_id`   INT NOT NULL,
+    `creator_id`        INT NOT NULL,
     `subscriber_id`     INT NOT NULL,
-    `status` ENUM('PENDING', 'ACCEPTED', 'REJECTED') NOT NULL DEFAULT 'PENDING',
+    `status`            ENUM('PENDING', 'ACCEPTED', 'REJECTED') NOT NULL DEFAULT 'PENDING',
     PRIMARY KEY (`creator_id`, `subscriber_id`)
 );
 
 CREATE TABLE `database`.`logging`
 (
-    `id`   INT AUTO_INCREMENT NOT NULL,
-    `description`   VARCHAR(256) NOT NULL,
-    `IP`   VARCHAR(16) NOT NULL,
-    `endpoint`   VARCHAR(256) NOT NULL,
-    `requested_at`   TIMESTAMP NOT NULL,
+    `id`                INT AUTO_INCREMENT NOT NULL,
+    `description`       VARCHAR(256) NOT NULL,
+    `IP`                VARCHAR(16) NOT NULL,
+    `endpoint`          VARCHAR(256) NOT NULL,
+    `requested_at`      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 );
